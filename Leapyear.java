a7a4bfd5f1d6e7beae39c183d884d40709c5a4f0
Shariar Anuvob <anuvobbroh@gmail.com>
2026-09-24 02:14:24 +0600

    Leap Year

---
+++ Leapyear.java
@@ -0,0 +1,18 @@
+
+package vobba;
+import java.util.Scanner;
+public class Leapyear {
+     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = input.nextInt();
+
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println(year + " is a Leap Year.");
+        } else {
+            System.out.println(year + " is not a Leap Year.");
+        }
+    }
+}
